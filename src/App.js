3594bb33b5823d946ad0ef41add1a167de6021ee
Srima Bose <147753150+srimabose@users.noreply.github.com>
2025-02-21 12:46:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-
 import './App.css';
-import { Navbar } from './Components/Navbar/Navbar';
+import { Navbar } from './Components/Navbar/Navbar.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Shop } from './Pages/Shop';
-import { ShopCategory } from './Pages/ShopCategory';
-import { Product } from './Pages/Product';
-import { Cart } from './Pages/Cart';
-import { LoginSignup } from './Pages/LoginSignup';
-import { Footer } from './Components/Footer/Footer';
+import { Shop } from './Pages/Shop.jsx';
+import { ShopCategory } from './Pages/ShopCategory.jsx';
+import { Product } from './Pages/Product.jsx';
+import { Cart } from './Pages/Cart.jsx';
+import { LoginSignup } from './Pages/LoginSignup.jsx';
+import { Footer } from './Components/Footer/Footer.jsx';
 import men_banner from './Components/Assests/banner_mens.png'
 import women_banner from './Components/Assests/banner_women.png'
 import kids_banner from './Components/Assests/banner_kids.png'
